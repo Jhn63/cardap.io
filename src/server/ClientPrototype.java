e993391981840de8f4a23847329753f7d4425354
João Cirilo <jvscirilo12@gmail.com>
2023-05-23 21:30:33 -0300

    Prototipo de cliente para servidor de dados

--- src/server/ClientPrototype.java
+++ src/server/ClientPrototype.java
@@ -1,13 +1,15 @@
 package server;
 
-class ClientPrototype
+public class ClientPrototype
 {
     public String name;
     public int id;
+    public int[] data;
 
-    public ClientPrototype (String name, int id)
+    public ClientPrototype (String name, int id, int[] data)
     {
         this.name = name;
         this.id = id;
+        this.data = data;
     }
 }
